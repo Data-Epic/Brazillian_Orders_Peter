5aac0ea71fb9082ea52b6b0f670371c45198569f
unknown <peteradekolu@gmail.com>
2024-08-17 01:14:36 +0100

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e  # Exit immediately if a command exits with a non-zero status
 echo "Running tests..."
 python run_tests.py
 echo "Starting the application..."
-python src/main.py &
+python src/main.py 
 # echo "Keeping the container alive..."
 # tail -f /dev/null
