151c4e6a914644ad1837152c5677a6cfc8ce06ae
unknown <peteradekolu@gmail.com>
2024-11-13 12:17:52 +0100

    updated run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,13 @@
 import unittest
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    
+    test_suite = test_loader.discover("tests", pattern="test_*.py")
+
     runner = unittest.TextTestRunner(verbosity=2)
     result = runner.run(test_suite)
     sys.exit(not result.wasSuccessful())
