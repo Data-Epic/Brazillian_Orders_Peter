2e558b5538fc676d68946e13c77a45ce1d6a4966
unknown <peteradekolu@gmail.com>
2024-11-13 11:07:15 +0100

    updated src files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,4 @@
-import os
-import sys
 from src.api import app
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
